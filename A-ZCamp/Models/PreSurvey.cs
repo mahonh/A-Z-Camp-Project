fccbf1f2e25e5f92e2ae1966b39fdea9d50178f0
Chad Mahon <mahonh@marshall.edu>
2017-09-24 18:25:31 -0400

    Intial AppHarbor Test
    First test to see how AppHarbor works, web app still needs work and is only being used for AppHarbor

---
+++ A-ZCamp/Models/PreSurvey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace A_ZCamp.Models
+{
+    public class PreSurvey
+    {
+        public int ID { get; set; }
+        public String Email { get; set; }
+        public String WhatDoYouExpect { get; set; }
+        public String AreYouLookingForward { get; set; }
+    }
+}
