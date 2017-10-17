5284dc2f1fe8d224b0c5b7615b0620119debec19
Chad Mahon <mahonh@marshall.edu>
2017-10-16 23:58:10 -0400

    Still working on it...
    Changed several things, still not a working layout

--- A-ZCamp/App_Start/BundleConfig.cs
+++ A-ZCamp/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace A_ZCamp
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/cosmo.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
