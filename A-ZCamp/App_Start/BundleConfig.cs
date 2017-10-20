db019b411afc08e936db1aec5818dcff5adcb546
Chad Mahon <mahonh@marshall.edu>
2017-10-20 18:49:36 -0400

    Still working on it

--- A-ZCamp/App_Start/BundleConfig.cs
+++ A-ZCamp/App_Start/BundleConfig.cs
@@ -23,6 +23,38 @@ namespace A_ZCamp
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            // Bundle for original site's javascript at begining
+            bundles.Add(new ScriptBundle("~/bundles/MainSiteBeginJS").Include(
+                      "~/Content/JavaScript/common.js",
+                      "~/Content/JavaScript/util.js",
+                      "~/Content/JavaScript/stats.js"));
+
+            // Bundle for original site's javascript at end
+            bundles.Add(new ScriptBundle("~/bundles/MainSiteJS").Include(
+                      "~/Content/JavaScript/jquery-1.js",
+                      "~/Content/JavaScript/bootstrap.js",
+                      "~/Content/JavaScript/jquery_003.js",
+                      "~/Content/JavaScript/waypoints.js",
+                      "~/Content/JavaScript/js",
+                      "~/Content/JavaScript/gmaps.js",
+                      "~/Content/JavaScript/masonry.js",
+                      "~/Content/JavaScript/owl.js",
+                      "~/Content/JavaScript/jquery_004.js",
+                      "~/Content/JavaScript/jquery.js",
+                      "~/Content/JavaScript/jquery_002.js",
+                      "~/Content/JavaScript/font.js"));
+
+            // Bundle for original site's css
+            bundles.Add(new StyleBundle("~/bundles/MainSiteCSS").Include(
+                      "~/Content/Style/css.css",
+                      "~/Content/Style/font-awesome.css",
+                      "~/Content/bootstrap.css",
+                      "~/Content/Style/style.css",
+                      "~/Content/Style/custom.css",
+                      "~/Content/Style/owl.css",
+                      "~/Content/Style/owl_002.css",
+                      "~/Content/Style/animate.css"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
