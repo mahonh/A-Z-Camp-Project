5e00a7a5177191e595314cb275845d968a62b5a8
Chad Mahon <mahonh@marshall.edu>
2017-12-03 11:11:00 -0500

    Register fix

--- A-ZCamp/Controllers/AccountController.cs
+++ A-ZCamp/Controllers/AccountController.cs
@@ -139,12 +139,12 @@ namespace A_ZCamp.Controllers
         [AllowAnonymous]
         public ActionResult Register()
         {
-            /*
+            
             if (!User.Identity.IsAuthenticated)
             {
                 return RedirectToAction("Index", "Home");
             }
-            */
+            
 
             return View();
         }
